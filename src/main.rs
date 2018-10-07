18db0c918946e45c03bfd0e00ab559c94e67282c
feixianer <noemail@anybox.com>
2018-10-07 21:16:06 +0800

    initlize commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate tokio;
+
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use tokio::codec::BytesCodec;
+use tokio::codec::Decoder;
+
+fn main() {
+    let addr = "127.0.0.1:6142".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
+    println!("Listening on: {}", addr);
+
+    let server = listener.incoming()
+        .map_err(|e| println!("failed to accept socket; error = {:?}", e))
+        .for_each(|stream| {
+            println!("accepted socket; addr={:?}", stream.peer_addr().unwrap());
+            let frame = BytesCodec::new().framed(stream);
+            let (_, splitstream) = frame.split();
+            
+            let processor = splitstream.for_each(|bytes| {
+                println!("bytes: {:?}", bytes);
+                Ok(())
+            })
+            .and_then(|()| {
+                println!("Socket received FIN packet and closed connection.");
+                Ok(())
+            })
+            .or_else(|err| {
+                println!("socket closed with error: {:?}", err);
+                Err(err)
+            })
+            .then(|result| {
+                println!("socket closed with result: {:?}", result);
+                Ok(())
+            });
+
+            tokio::spawn(processor)
+        });
+
+    println!("server running on localhost:6142");
+
+    tokio::run(server);
+
+}
