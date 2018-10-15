f33bdb39e65c923998d745debaace49c5db8f43f
simacuo <noemail@anybox.com>
2018-10-15 14:54:53 +0800

    Write another place.

--- src/net.rs
+++ src/net.rs
@@ -1,17 +1,20 @@
 extern crate tokio;
 extern crate bytes;
 extern crate futures;
+extern crate mio;
 
 use tokio::net::{TcpListener, TcpStream};
 use std::net::SocketAddr;
 use std::io;
 use std::vec;
 use Socks5::Socks5Phase;
+use mio::Ready;
 
 struct Transfer {
     local: TcpStream,
     remote: TcpStream,
     buf: Vec<u8>,
+    idx: usize,
     phase: Socks5Phase,
 }
 
@@ -20,6 +23,7 @@ impl Transfer {
         Transfer {
             local,
             buf: Vec::with_capacity(2048),
+            idx: 0,
             phase: Socks5Phase::Initialize,
         }
     }
@@ -32,9 +36,13 @@ impl Future for Transfer {
     fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error>
         match phase {
             Socks5Phase::Initlize {
-                let ready = self.local.poll_read_ready()?;
+                let ready: Async<Ready> = self.local.poll_read_ready(Ready::readable())?
                 if ready.is_not_ready(self) {
                     return Ok(Async::NotReady);
                 }
+
+                let len = self.buf.capacity() - self.buf.len();
+
+
                 
 
