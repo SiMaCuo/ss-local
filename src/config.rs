b2cbe7bf0e313c22f70f8775d8beea49449a1781
simacuo <lenger81@hotmail.com>
2021-02-14 10:20:29 +0800

    remove configuration items that are no longer needed

--- src/config.rs
+++ src/config.rs
@@ -8,18 +8,15 @@ use std::{
     io::{Error, ErrorKind},
     net::{IpAddr, Ipv4Addr, SocketAddr},
     path::{Path, PathBuf},
-    time::Duration,
 };
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct ConfigJson {
     local_port: u16,
-    local_threadpool_size: usize,
     server_ip: String,
     server_port: u16,
     password: String,
     method: String,
-    keepalive: u64,
     acl: String,
 }
 
@@ -35,11 +32,9 @@ impl ConfigJson {
 
 pub struct SsConfig {
     local_addr: SocketAddr,
-    local_threadpool_size: usize,
     server_addr: SocketAddr,
     enc_key: Bytes,
     method: CipherMethod,
-    keeplive: Option<Duration>,
     #[cfg(target_os = "windows")]
     acl: Acl,
     dir: PathBuf,
@@ -50,11 +45,6 @@ impl SsConfig {
         let json = ConfigJson::new(path)?;
         let local_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), json.local_port);
         let server_addr = format!("{}:{}", json.server_ip, json.server_port).parse().unwrap();
-        let keeplive = if json.keepalive == 0 {
-            None
-        } else {
-            Some(Duration::from_secs(json.keepalive))
-        };
 
         let mut acl_path = dir.clone();
         acl_path.push(json.acl);
@@ -71,11 +61,9 @@ impl SsConfig {
         #[cfg(target_os = "windows")]
         let mut s = SsConfig {
             local_addr,
-            local_threadpool_size: json.local_threadpool_size,
             server_addr,
             enc_key: CipherMethod::derive_key(json.password.as_bytes(), 32),
             method: json.method.parse().unwrap(),
-            keeplive,
             #[cfg(target_os = "windows")]
             acl: Acl::new(),
             dir,
@@ -86,11 +74,9 @@ impl SsConfig {
         #[cfg(target_os = "linux")]
         let s = SsConfig {
             local_addr,
-            local_threadpool_size: json.local_threadpool_size,
             server_addr,
             enc_key: CipherMethod::derive_key(json.password.as_bytes(), 32),
             method: json.method.parse().unwrap(),
-            keeplive,
             dir,
         };
 
@@ -99,10 +85,6 @@ impl SsConfig {
 
     pub fn listen_addr(&self) -> SocketAddr {
         self.local_addr
-    }
-
-    pub fn romio_threadpool_size(&self) -> usize {
-        self.local_threadpool_size
     }
 
     pub fn ss_server_addr(&self) -> SocketAddr {
@@ -115,10 +97,6 @@ impl SsConfig {
 
     pub fn method(&self) -> CipherMethod {
         self.method
-    }
-
-    pub fn keepalive(&self) -> Option<Duration> {
-        self.keeplive
     }
 
     #[cfg(target_os = "windows")]
